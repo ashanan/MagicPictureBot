1b3e130c87dd790092994ba9a09d73691be72228
Avner Shanan <ashanan@gmail.com>
2016-09-02 15:50:28 -0500

    hello world!

---
+++ bot.rb
@@ -0,0 +1,4 @@
+require_relative 'config.rb'
+
+client = Configuration.twitter_client
+client.update("Magic is coming soon!")
--- config.example.rb
+++ config.example.rb
@@ -1,8 +1,19 @@
-DPLibrary.api_key = '<API KEY GOES HERE>'
+require 'DPLibrary'
+require 'Twitter'
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "YOUR_CONSUMER_KEY"
-  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-  config.access_token        = "YOUR_ACCESS_TOKEN"
-  config.access_token_secret = "YOUR_ACCESS_SECRET"
+DPLibrary.api_key = 'YOUR_DPLA_API_KEY'
+
+module Configuration
+
+	def self.twitter_client
+		client = Twitter::REST::Client.new do |config|
+		  config.consumer_key        = "YOUR_CONSUMER_KEY"
+		  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+		  config.access_token        = "YOUR_ACCESS_TOKEN"
+		  config.access_token_secret = "YOUR_ACCESS_SECRET"
+		end
+		
+		client
+	end
+
 end
