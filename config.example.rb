9813f18fe9af8e55f49e616fde99996d07caf10f
Avner Shanan <ashanan@gmail.com>
2016-09-02 15:07:46 -0500

    Add twitter biz

--- config.example.rb
+++ config.example.rb
@@ -1,1 +1,8 @@
 DPLibrary.api_key = '<API KEY GOES HERE>'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "YOUR_CONSUMER_KEY"
+  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+  config.access_token        = "YOUR_ACCESS_TOKEN"
+  config.access_token_secret = "YOUR_ACCESS_SECRET"
+end
